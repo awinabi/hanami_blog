09c0198b97c0f9a1bfd3eb063049c443ed6ae9ed
Awin Abi <awin@awinawin.awin>
2018-05-30 18:38:27 +0530

    Index action for listing blog posts

---
+++ apps/web/controllers/posts/index.rb
@@ -0,0 +1,12 @@
+module Web::Controllers::Posts
+  class Index
+    include Web::Action
+
+    expose :posts
+
+    def call(params)
+      repository = PostRepository.new
+      @posts = repository.all
+    end
+  end
+end
